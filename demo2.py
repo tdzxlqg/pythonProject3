03855bd9411515aae31e8cba897f1636171c87b6
tdzxlqg <tdzxlqg@163.com>
2020-12-27 20:32:32 +0800

    add demo2

---
+++ demo2.py
@@ -0,0 +1,25 @@
+class Animal:
+    def __init__(self):
+        print('Animal init')
+class Bird(Animal):
+    def __init__(self, hungry):
+        super().__init__()
+        self.hungry = hungry
+    def eat(self):
+        if self.hungry:
+            print('已经吃了虫子了，不饿了！')
+            self.hungry = False
+        else:
+            print("已经吃过饭了，不饿了！")
+b = Bird(True)
+b.eat()
+b.eat()
+class SongBird(Bird):
+    def __init__(self,hungry):
+        super(SongBird,self).__init__(hungry)
+        self.sound = '向天再借五百年'
+    def sing(self):
+        print(self.sound)
+sb=SongBird(True)
+sb.sing()
+sb.eat()
